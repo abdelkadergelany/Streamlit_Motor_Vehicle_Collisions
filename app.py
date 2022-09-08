821eecd32e9bf120aa30dca725dfb881b12a40a0
aly-abdelkader-iut-cse-4301 <abdelkadergelany25@gmail.com>
2022-09-08 20:10:14 +0900

    adding 3D markers

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
 
 DATA_URL= "Motor_Vehicle_Collisions.csv"
 
@@ -27,6 +28,32 @@ st.map(data.query("number_of_persons_injured>= @injured_people")[['latitude','lo
 st.header("How many collisions occur during a given time of day?")
 hour = st.slider("Hour to look at", 0,23)
 data =  data[data["date_time"].dt.hour == hour]
+
+st.markdown("Vehicle collision betweem %i:00 and %i:00" % (hour,(hour+1)%24))
+
+
+midpoint = (np.average(data['latitude']),np.average(data['longitude']))
+
+st.write(pdk.Deck(
+map_style="mapbox://styles/mapbox/light-v9",
+initial_view_state={
+"latitude":midpoint[0],
+"longitude":midpoint[1],
+"zoom":11,
+"pitch":50,
+},
+layers = [
+pdk.Layer(
+"HexagonLayer",
+data=data[['date_time','latitude', 'longitude']],
+get_position = ['longitude','latitude'],
+radius =  True,
+extruded = True,
+pickable=True,
+elevation_range = [0,1000],
+)
+]
+))
 
 
 if st.checkbox("Show row Data", False):
